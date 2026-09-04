9c2c800b3ee52fa3eebbf2d8796ec98540b7dfa3
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-09-04 12:22:20 -0500

    Load latest competition dashboard assets

--- auth.js
+++ auth.js
@@ -31,7 +31,7 @@
 
     const loadApp = () => {
       const script = document.createElement("script");
-      script.src = "app.js?v=20260904-4";
+      script.src = "app.js?v=20260904-5";
       script.dataset.dashboardApp = "true";
       document.body.appendChild(script);
     };
