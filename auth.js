0953870b65a581fa9fa4cc73888cf761cd059ecf
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-09-04 17:31:11 -0500

    Load chart resize hotfix

--- auth.js
+++ auth.js
@@ -31,7 +31,7 @@
 
     const loadApp = () => {
       const script = document.createElement("script");
-      script.src = "app.js?v=20260904-15";
+      script.src = "app.js?v=20260904-16";
       script.dataset.dashboardApp = "true";
       document.body.appendChild(script);
     };
