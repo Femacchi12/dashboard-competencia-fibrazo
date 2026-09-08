d48e544ebc7ba783081939c3b71e287287fbe314
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-09-08 17:38:50 -0500

    Refresh dashboard app cache version

--- auth.js
+++ auth.js
@@ -31,7 +31,7 @@
 
     const loadApp = () => {
       const script = document.createElement("script");
-      script.src = "app.js?v=20260907-5";
+      script.src = "app.js?v=20260908-1";
       script.dataset.dashboardApp = "true";
       document.body.appendChild(script);
     };
