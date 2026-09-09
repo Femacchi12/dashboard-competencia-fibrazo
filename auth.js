aa69dbded7e09a60271d38f3181cc7300036b2fd
Eduardo Macchi <fernandoemacchi@gmail.com>
2026-09-09 12:27:06 -0500

    Refresh app cache for chart label and tab cleanup changes

--- auth.js
+++ auth.js
@@ -29,7 +29,7 @@
   const loadDashboard = () => {
     if (document.querySelector('script[data-dashboard-app]')) return;
     const script = document.createElement("script");
-    script.src = "js/app.js?v=20260909-5";
+    script.src = "js/app.js?v=20260909-6";
     script.dataset.dashboardApp = "true";
     document.body.appendChild(script);
   };
